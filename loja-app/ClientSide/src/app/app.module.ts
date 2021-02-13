7385dd3503f2ed59506ef894bf34d7fac1c18482
frederico101 <fredyalves.fredyalves@hotmail.com>
2021-02-13 01:26:03 -0300

    Inicio do site

---
+++ loja-app/ClientSide/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './components/view/header/header.component'; 
+import { BodyComponent } from './components/view/body/body.component'; 
+import { FooterComponent } from './components/view/footer/footer.component'; 
+import { SideNavComponent } from './components/view/side-nav/side-nav.component'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule 
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
